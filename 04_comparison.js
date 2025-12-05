592a0b53c7e74da0419af1553de5fac243b0e416
com-engineer <2022.gaurav.gupta@ves.ac.in>
2025-12-05 22:19:16 +0530

    completed comparison topic........

--- 04_comparison.js
+++ 04_comparison.js
@@ -1,10 +1,21 @@
 //confusing comparisons 
-
+//comparing the same datatype is easier but create confuse when we need to compare different datatypes
 console.log("2">1);
 console.log("02">1);
 
 console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+console.log(null==0);//equality check and other comparison operator works differently
+console.log(null>=0);//comparison convert null as 0 number while equality don't 
+
+console.log(undefined==0);
+console.log(undefined>0);// all gives false
+console.log(undefined>=0);
+
+
+//Strict checking using "===" it not only checks the "value" but also checks the "datatype"
+
+console.log(1==="1");//false
+console.log(1=="1");//true
+
 
 
