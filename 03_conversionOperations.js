6096c24cef8312a3ffc46258d7e7598ea0dca655
com-engineer <2022.gaurav.gupta@ves.ac.in>
2025-12-05 22:08:14 +0530

    small update in conversion Operation......

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -10,12 +10,22 @@
 
 // //but if we want to use score as number then we have to convert it into number datatype
 
-// let valueInNumber= Number(score5);//using Number() function
+// let valueInNumber1= Number(score);//=>20
+// let valueInNumber2= Number(score1);//=>NaN
+// let valueInNumber3= Number(score2);//=>0
+// let valueInNumber4= Number(score3);//=>NaN
+// let valueInNumber5= Number(score4);//=>1
+// let valueInNumber6= Number(score5);//=>Nan
 // console.log(typeof valueInNumber);
 
 // // but score1 is not a number
 // console.log(valueInNumber);//=>Nan=>not a number
-
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+// console.log(valueInNumber5);
+// console.log(valueInNumber6);
 // let isLogedIn=1;
 // let isLogedIn1="";
 // let isLogedIn2="gaurav";
@@ -33,22 +43,22 @@
 
 //***********************Operations****************** */
 
-let val=3;
-let negVal=-val;
-console.log(negVal);
+// let val=3;
+// let negVal=-val;
+// console.log(negVal);
 
-let str1="hello";
-let str2="gaurav";
-let str3=str1+ " "+ str2;
-console.log(str3);
+// let str1="hello";
+// let str2="gaurav";
+// let str3=str1+ " "+ str2;
+// console.log(str3);
 
-console.log("1"+2);//=>12
-console.log(1+"2");//=>12
-console.log(1+2+"2");//=>32 if string is present at last then previous will be added as number
-console.log("1"+2+2);//=>122  if string is present at starting then all will be converted to string
+// console.log("1"+2);//=>12
+// console.log(1+"2");//=>12
+// console.log(1+2+"2");//=>32 if string is present at last then previous will be added as number
+// console.log("1"+2+2);//=>122  if string is present at starting then all will be converted to string
 
-console.log(true);//true
-console.log(+true);//1
+// console.log(true);//true
+// console.log(+true);//1
 
 // /If used postfix, with operator after operand (for example, x++), the increment operator increments
 //  and returns the value before incrementing.
