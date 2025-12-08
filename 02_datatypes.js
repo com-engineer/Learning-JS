37317042a34e8622fa10dd5f9ccb336dccaf3a29
com-engineer <2022.gaurav.gupta@ves.ac.in>
2025-12-08 19:10:01 +0530

    summary of data types.......

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -17,3 +17,34 @@
 console.log(typeof("hitesh"));
 console.log(typeof(null));//=>object (bug in js)
 console.log(typeof undefined);//=>undefined
+
+
+//Data is categorize on the basis how it is tored and access in the memory
+//there are two types of datatypes
+//1. "Primitive datatypes" => stored in stack memory
+//used as call by value
+//eg: number, string, boolean, undefined, null, (symbol=>to make some variable unique), bigInt=>7 types
+//changes are made to copy of original value not to original value
+//eg of symbol let id=Symbol("123");
+
+//JavaScript is a dynamically typed language
+//we don't need to specify datatype while declaring variable
+//datatype is automatically assigned when we assign value to variable
+//datatype of variable can be changed during re-assignment
+
+//2. "Reference datatypes" => stored in heap memory
+//used as call by reference
+//eg: objects, arrays, functions => 3 types
+//changes are made to original value
+
+//return types of data types
+// console.log(typeof 1234);//number
+// console.log(typeof 1234567890123456789012345678901234567890n);//bigInt
+// console.log(typeof "hitesh");//string
+// console.log(typeof true);//boolean
+// console.log(typeof undefined);  //undefined
+// console.log(typeof null); //object
+// console.log(typeof {name:"hitesh",age:24});//object
+// console.log(typeof [1,2,3,4,5]);//object
+// console.log(typeof function(){})//function
+//function is special datatype which returns function itself
